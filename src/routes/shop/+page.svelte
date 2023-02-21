d141de18f35a79a54d9b170f553bcdb54bbc3cd1
murraycopps <murraycopps@gmail.com>
2023-02-21 10:30:22 -0500

    added images to the shop cards

--- src/routes/shop/+page.svelte
+++ src/routes/shop/+page.svelte
@@ -6,12 +6,12 @@
 	export let data: { items: Item[] };
 </script>
 
-<section class="flex flex-col">
-	<!-- create a section to shop shoes gear or clothes -->
-	<div class="grid grid-cols-1 gap-4 p-4 sm:grid-cols-2 md:grid-cols-3 ">
-		<ShopCards type="shoes" />
-		<ShopCards type="gear" />
-		<ShopCards type="clothes" />
+<section class="flex flex-col gap-4 p-4">
+	<h1 class="text-6xl text-center">Shop</h1>
+	<div class="grid grid-cols-1 gap-8 p-4 sm:grid-cols-2 md:grid-cols-3 ">
+		<ShopCards type="shoes" image="https://ccwh54no.cdn.imgeng.in/media/catalog/product/cache/f9298953e225fe5aa69348b5b4218d7e/1/0/1013A116_400_SR_RT_GLB_PNG_1280x1280_PNG_png.png" />
+		<ShopCards type="gear" image="https://media.wired.co.uk/photos/606d9c06307b8f0b37c724f4/master/w_1600%2Cc_limit/wired-uk-hero-gps-watches-3.jpg" />
+		<ShopCards type="clothes" image="https://tracksmith-media.imgix.net/Summer22-Mens-OTQ_Singlet_with_Woven_Label.png?bg=f8f8f8&fit=fillmax&w=1080&h=1080"/>
 		
 	</div>
 	<div class="grid grid-cols-1 gap-4 p-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5">
--- src/routes/shop/ShopCards.svelte
+++ src/routes/shop/ShopCards.svelte
@@ -1,10 +1,20 @@
 <script lang="ts">
 	export let type: string;
+	export let image: string;
 
 	const capitalize = (str: string) => str.charAt(0).toUpperCase() + str.slice(1);
 </script>
 
-<a href={`/shop/${type}`}>
-	<h2>{capitalize(type)}</h2>
-	<p>Shop our selection of {type}</p>
+<a href={`/shop/${type}`} class="relative h-full">
+	<img src={image} alt={type} class="object-cover bg-green-200 rounded-3xl aspect-square" />
+	<div class="absolute inset-0 flex flex-col items-center justify-center gap-4 p-4 text-white z-100">
+		<h2 class="text-5xl">{capitalize(type)}</h2>
+		<p class="text-2xl">Shop our selection of {type}</p>
+	</div>
 </a>
+
+<style>
+    img{
+        filter: blur(.4rem) brightness(0.65);
+    }
+</style>
