e4b455cab95bafa39d776e4a7852c19737405800
murraycopps <murraycopps@gmail.com>
2023-02-22 18:45:19 -0500

    styled dropdown

--- src/routes/shop/NavBarContent.svelte
+++ src/routes/shop/NavBarContent.svelte
@@ -4,23 +4,25 @@
 	import { clickOutside } from '$lib/scripts/clickOutside';
 
 	export let open: boolean;
-    export let close: () => void;
+	export let close: () => void;
 </script>
 
 <div
 	class={`fixed top-0 bottom-0 left-0 text-white text-xl text-center bg-gray-500 overflow-hidden ${
 		open ? 'w-1/3' : 'w-0'
 	}`}
-    id="nav-bar"
-	use:clickOutside={{ignore: 'nav-button'}} on:click_outside={close}
-    
+	id="nav-bar"
+	use:clickOutside={{ ignore: 'nav-button' }}
+	on:click_outside={close}
 >
 	<nav class="flex flex-col items-center w-full h-full gap-4 p-4">
-		<a href="/shop/clothes" class="w-1/2">Clothes</a>
+		<a href="/shop" on:click={close}>Shop All</a>
 		<span class="w-3/4 mx-auto border-b border-white" />
-		<a href="/shop/gear">Gear</a>
+		<NavBarShoeDropdown {close} />
 		<span class="w-3/4 mx-auto border-b border-white" />
-		<NavBarShoeDropdown />
+		<a href="/shop/clothes" on:click={close}>Clothes</a>
+		<span class="w-3/4 mx-auto border-b border-white" />
+		<a href="/shop/gear" on:click={close}>Gear</a>
 	</nav>
 </div>
 
--- src/routes/shop/NavBarShoeDropdown.svelte
+++ src/routes/shop/NavBarShoeDropdown.svelte
@@ -1,20 +1,51 @@
-<script>
+<script lang="ts">
 	let open = false;
+	export let close: () => void;
 </script>
 
-<div>
-	<button class="relative" on:click={() => (open = !open)}> Shoes </button>
-	<div class="hidden w-full" class:open>
+<div class="relative w-full flex flex-col items-center" class:open>
+	<button class="relative w-3/4" on:click={() => (open = !open)}>
+		Shoes
+		<span class="absolute right-0 top-1/2 aspect-square h-full transform -translate-y-1/2">
+			<svg class="w-full h-full" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+				<path
+					d="M7 10L12 15L17 10"
+					stroke="white"
+					stroke-width="2"
+					stroke-linecap="round"
+					stroke-linejoin="round"
+				/>
+			</svg>
+		</span>
+	</button>
+	<div class="hidden w-full flex-col items-center px-4 gap-2 absolute top-full z-50 bg-gray-500">
 		<span class="w-3/4 mx-auto border-b border-white" />
-		<a href="/shop/shoes">All</a>
-		<a href="/shop/shoes/trail">Trail</a>
-		<a href="/shop/shoes/road">Road</a>
-		<a href="/shop/shoes/spikes">Spikes</a>
+		<a href="/shop/shoes" on:click={close}>Shop All Shoes</a>
+		<span class="w-3/4 mx-auto border-b border-white" />
+		<a href="/shop/shoes/trail" on:click={close}>Trail</a>
+		<span class="w-3/4 mx-auto border-b border-white" />
+		<a href="/shop/shoes/road" on:click={close}>Road</a>
+		<span class="w-3/4 mx-auto border-b border-white" />
+		<a href="/shop/shoes/spikes" on:click={close}>Spikes</a>
+		<span class="wider mx-auto border-b border-white" />
 	</div>
 </div>
 
 <style>
-	.open {
-		display: block;
+	.open{
+		padding-bottom: 1rem;
+	}
+	.open > div {
+		display: flex;
+	}
+
+	.open > button > span > svg {
+		transform: rotate(180deg);
+	}
+	svg {
+		transition: transform 0.3s ease-in-out;
+	}
+	.wider {
+		width: calc(3 / 4 * (100% + 2rem))
 	}
 </style>
