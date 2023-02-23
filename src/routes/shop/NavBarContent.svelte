6b803ce8a108ba97b7623e5c79a2278d635d64c5
murraycopps <murraycopps@gmail.com>
2023-02-23 15:40:48 -0500

    some navbar work

--- src/routes/shop/NavBarContent.svelte
+++ src/routes/shop/NavBarContent.svelte
@@ -1,10 +1,17 @@
 <script lang="ts">
-	import { onDestroy, onMount } from 'svelte';
 	import NavBarShoeDropdown from './NavBarShoeDropdown.svelte';
 	import { clickOutside } from '$lib/scripts/clickOutside';
 
 	export let open: boolean;
 	export let close: () => void;
+	let dropdownOpen: boolean = false;
+	let toggleDropdown = () => {
+		dropdownOpen = !dropdownOpen;
+	};
+	let closeBoth = () => {
+		dropdownOpen = false;
+		close();
+	};
 </script>
 
 <div
@@ -13,12 +20,12 @@
 	}`}
 	id="nav-bar"
 	use:clickOutside={{ ignore: 'nav-button' }}
-	on:click_outside={close}
+	on:click_outside={closeBoth}
 >
 	<nav class="flex flex-col items-center w-full h-full gap-4 p-4">
 		<a href="/shop" on:click={close}>Shop All</a>
 		<span class="w-3/4 mx-auto border-b border-white" />
-		<NavBarShoeDropdown {close} />
+		<NavBarShoeDropdown close={closeBoth} open={dropdownOpen} {toggleDropdown} />
 		<span class="w-3/4 mx-auto border-b border-white" />
 		<a href="/shop/clothes" on:click={close}>Clothes</a>
 		<span class="w-3/4 mx-auto border-b border-white" />
--- src/routes/shop/NavBarShoeDropdown.svelte
+++ src/routes/shop/NavBarShoeDropdown.svelte
@@ -1,14 +1,11 @@
 <script lang="ts">
-	let open = false;
+	export let open: boolean;
 	export let close: () => void;
-	const closeBoth = () => {
-		close();
-		open = false;
-	};
+	export let toggleDropdown: () => void;
 </script>
 
 <div class="relative flex flex-col items-center w-full" class:open>
-	<button class="relative w-3/4" on:click={() => (open = !open)}>
+	<button class="relative w-3/4" on:click={toggleDropdown}>
 		Shoes
 		<span class="absolute right-0 h-full transform -translate-y-1/2 top-1/2 aspect-square">
 			<svg class="w-full h-full" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -24,13 +21,13 @@
 	</button>
 	<div class="absolute z-50 flex-col items-center hidden w-full gap-2 px-4 bg-gray-500 top-full">
 		<span class="w-3/4 mx-auto border-b border-white" />
-		<a href="/shop/shoes" on:click={closeBoth}>Shop All Shoes</a>
+		<a href="/shop/shoes" on:click={close}>Shop All Shoes</a>
 		<span class="w-3/4 mx-auto border-b border-white" />
-		<a href="/shop/shoes/trail" on:click={closeBoth}>Trail</a>
+		<a href="/shop/shoes/trail" on:click={close}>Trail</a>
 		<span class="w-3/4 mx-auto border-b border-white" />
-		<a href="/shop/shoes/road" on:click={closeBoth}>Road</a>
+		<a href="/shop/shoes/road" on:click={close}>Road</a>
 		<span class="w-3/4 mx-auto border-b border-white" />
-		<a href="/shop/shoes/spikes" on:click={closeBoth}>Spikes</a>
+		<a href="/shop/shoes/spikes" on:click={close}>Spikes</a>
 		<span class="mx-auto border-b border-white wider" />
 	</div>
 </div>
